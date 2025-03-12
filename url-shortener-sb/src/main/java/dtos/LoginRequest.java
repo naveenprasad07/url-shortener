2fc09aec0384050ddc50101180ab300c53e18045
naveenprasad07 <naveenacad89@gmail.com>
2025-03-12 20:08:44 +0530

    initial-commit

---
+++ url-shortener-sb/src/main/java/dtos/LoginRequest.java
@@ -0,0 +1,11 @@
+package dtos;
+
+import lombok.Data;
+
+
+
+@Data
+public class LoginRequest {
+    private String username;
+    private String password;
+}
---
+++ url-shortener-sb/t.md
@@ -0,0 +1,2 @@
+Challenges Faced:
+- Took me Two Hours to debug a connection error between database and java and the error is due to the privilege issues
